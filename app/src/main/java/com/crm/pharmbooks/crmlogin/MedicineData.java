540313bfd25ae6c6dbd427cbe9906b72af95231c
Saurabh Sharma <saurabhsharma2894@gmail.com>
2017-08-19 14:49:00 +0530

    Update MedicineData.java

--- app/src/main/java/com/crm/pharmbooks/crmlogin/MedicineData.java
+++ app/src/main/java/com/crm/pharmbooks/crmlogin/MedicineData.java
@@ -1,5 +1,8 @@
 package com.crm.pharmbooks.crmlogin;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -14,8 +17,25 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Vector;
 
 import Adapters.MedicineAdapter;
 import Model.MedicineDetail;
@@ -26,6 +46,162 @@ public class MedicineData extends AppCompatActivity {
     private RecyclerView recyclerView;
     private MedicineAdapter mAdapter;
     private FloatingActionButton fab;
+    private EditText MedicineName, MedicineQuantity;
+    private Button addButton;
+    String MedicineName_value,MedicineQuantity_value;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medicine_data);
+        MedicineName = (EditText)findViewById(R.id.MedicineName);
+        MedicineQuantity = (EditText) findViewById(R.id.MedicineQuantity);
+        addButton = (Button) findViewById(R.id.addButtonId);
+
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MedicineName_value = MedicineName.getText().toString();
+                MedicineQuantity_value = MedicineQuantity.getText().toString();
+                MedicineDetail detail = new MedicineDetail(MedicineName_value,Integer.parseInt(MedicineQuantity_value));
+                medicineDetailList.add(detail);
+                mAdapter.notifyDataSetChanged();
+            }
+        });
+
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        mAdapter = new MedicineAdapter(medicineDetailList);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(mAdapter);
+
+
+        /*fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View view) {
+
+                MedicineDetail medicineDetail;
+                medicineDetail = mAdapter.getData();
+                medicineDetailList.add(medicineDetail);
+                Log.d("tag",medicineDetailList.get(medicineDetailList.size()-1).getMName());
+                mAdapter.notifyDataSetChanged();
+            }
+
+        });*/
+    }
+    /*public static ArrayList<MedicineDetail> getListdata() {
+        JSONObject responseDetailsJSON = new JSONObject();
+        JSONArray jsonArray = new JSONArray();
+        //HashMap<String,Integer> medicineMap = new HashMap<?,?>();
+
+        ArrayList<MedicineDetail> medicineList = new Vector<MedicineDetail>(medicineMap.keySet().size());
+
+        for(MedicineDetail key: medicineMap.keySet()) {
+            medicineList.add(key);
+            JSONObject medicineDetailsJSON = new JSONObject();
+            medicineDetailsJSON.key.getMName();
+            medicineDetailsJSON.key.getMQuantity();
+            jsonArray.add(medicineDetailsJSON);
+        }
+        responseDetailsJSON.put("medicine",jsonArray);
+        return medicineList;
+
+    }*/
+
+
+    public JSONObject getJsonFromMyFormObject(ArrayList<MedicineDetail> medicineDetailList) throws JSONException {
+        JSONObject responseDetailsJson = new JSONObject();
+        JSONArray jsonArray = new JSONArray();
+        try {
+            for (int i = 0; i < medicineDetailList.size(); i++) {
+                JSONObject formDetailsJson = new JSONObject();
+                formDetailsJson.put("medicine_"+(i+1), medicineDetailList.get(i).getMName());
+                formDetailsJson.put("days_"+(i+1), String.valueOf(medicineDetailList.get(i).getMQuantity()));
+
+                jsonArray.put(formDetailsJson);
+
+
+            }
+
+        }
+        catch (JSONException e) {
+            e.printStackTrace();
+        }
+        responseDetailsJson.put("medicineDetailList", jsonArray);
+
+        Log.d("mytag",responseDetailsJson+"");
+        return responseDetailsJson;
+
+
+    }
+
+    public void sendR() {
+        String url = "https://pharmcrm.herokuapp.com/api/save/";
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        int result;
+                        //String msg = null;
+                        try {
+                            JSONObject object = new JSONObject(response);
+                            //msg = object.getString("msg");
+                            result= object.getInt("result");
+                            if(result == 1)
+                            {
+
+
+
+
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        Toast.makeText(MedicineData.this,response,Toast.LENGTH_LONG).show();
+                        //Toast.makeText(MedicineData.this,msg+""+ res +"",Toast.LENGTH_LONG).show();
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MedicineData.this,error.toString(),Toast.LENGTH_LONG).show();
+                    }
+                }){
+            @Override
+            protected Map<String,String> getParams(){
+                Map<String, String> params = new HashMap<>();
+                try {
+                params.put("Cname","manya");
+                params.put("Cnumber","9898876545");
+                params.put("Cadd","delhi");
+                params.put("counter",String.valueOf(medicineDetailList.size()));
+                params.put("data",getJsonFromMyFormObject(medicineDetailList)+"");
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                return params;
+            }
+
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+
+    }
+
+
+
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -41,7 +217,7 @@ public class MedicineData extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
+        //Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
@@ -49,13 +225,20 @@ public class MedicineData extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.saveIcon) {
 
-            Log.d("hello","hello");
-            /*new View.OnClickListener() {
+            Log.d("mtag","hello");
+            sendR();
+            /*JSONObject json = null;
+            try {
+                json = getJsonFromMyFormObject(medicineDetailList);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            Log.d("json",json+"");
+            new View.OnClickListener() {
                 public void onClick(View view) {
-                            Log.v("EditText", medicineDetailList.getText().toString());
+
                 }
             };*/
-
 
             return true;
         }
@@ -63,58 +246,9 @@ public class MedicineData extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_medicine_data);
-
-
-        
 
 
 
-        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-
-
-
-
-
-
-
-        mAdapter = new MedicineAdapter(medicineDetailList);
-        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
-        recyclerView.setLayoutManager(mLayoutManager);
-        recyclerView.setItemAnimator(new DefaultItemAnimator());
-        recyclerView.setAdapter(mAdapter);
-        fab = (FloatingActionButton) findViewById(R.id.fab);
-        //prepareMedicineData();
-        fab.setOnClickListener(new View.OnClickListener() {
-
-
-            @Override
-            public void onClick(View view) {
-
-                MedicineDetail medicineDetail = new MedicineDetail("",10);
-                medicineDetailList.add(medicineDetail);
-                mAdapter.notifyDataSetChanged();
-                //medicineDetail.getMName().toString();
-                //Log.v("EditText",medicineDetail.getMQuantity().toString()+"");
-
-            }
-
-        });
-
-    }
-    //
-
-    /*private void prepareMedicineData() {
-
-        MedicineDetail medicineDetail = new MedicineDetail("Crocin",20);
-        medicineDetailList.add(medicineDetail);
-
-
-        mAdapter.notifyDataSetChanged();
-    }*/
 
 
 }
